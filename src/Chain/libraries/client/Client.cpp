eee8eb76a1bbbf48f51810578a7bf2a83a1792dd
yangsai <yangsai@new4g.cn>
2018-01-23 17:21:58 +0800

    fix data_path name

--- src/Chain/libraries/client/Client.cpp
+++ src/Chain/libraries/client/Client.cpp
@@ -403,7 +403,7 @@ namespace thinkyoung {
                         return dir_name;
                     };
 
-                    datadir = fc::app_path() / get_os_specific_dir_name("rrrr");
+                    datadir = fc::app_path() / get_os_specific_dir_name(ALP_BLOCKCHAIN_NAME);
                 }
                 return datadir;
 
