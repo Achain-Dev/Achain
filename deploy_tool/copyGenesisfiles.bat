7147f6feb8b18493d80fd7c355746e76b2d01ad0
WillAchain <33215404+WillAchain@users.noreply.github.com>
2018-01-23 19:52:05 +0800

    add some instructions
    add some instructions

--- deploy_tool/copyGenesisfiles.bat
+++ deploy_tool/copyGenesisfiles.bat
@@ -1,4 +1,4 @@
-:::: move files
+:::: this file is to help move files into Achain/src corresponding directory
 @echo please input the sourcecode solution directory 
 xcopy /y  /f  GenesisJson.cpp   %1\Chain\libraries\blockchain\GenesisJson.cpp
 xcopy /y  /f  GenesisJson_test.cpp  %1\Chain\libraries\blockchain\GenesisJson_test.cpp
